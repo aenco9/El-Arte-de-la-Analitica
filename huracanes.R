dd5861b0b6f577a630aae985fb9fff618ac6fda1
aenco9 <alex.enriquezco@gmail.com>
2020-10-27 20:01:35 -0600

    clase 2

--- huracanes.R
+++ huracanes.R
@@ -1,2 +1,13 @@
 huracanes <- read.csv("El Arte de la Analitica/HuracanesETL.csv")
-View(huracanes)
+summary(huracanes)
+library(dplyr)
+huracanesLimpio=filter(huracanes, huracanes$Population>0, huracanes$Presion>0, huracanes$Time>0)
+any(is.na(huracanesLimpio)) #No hay NA
+#Datos originales
+summary(huracanes)
+# Datos limpios
+summary(huracanesLimpio)
+plot(huracanesLimpio$Population, huracanesLimpio$CO2)
+plot(huracanesLimpio$CO2, huracanesLimpio$Duracion)
+plot(huracanesLimpio$CO2, huracanesLimpio$Presion)
+#Había muchos datos sucios como de Tiempo y población = 0 o de presión negativa.
